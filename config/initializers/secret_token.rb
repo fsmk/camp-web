92c0b1402fbf3b8927688cf31d56fea8ade1d1d5
PallaviTS <pallavi046@gmail.com>
2014-04-27 14:40:55 +0530

    Camp: Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FsmkCampWebsite::Application.config.secret_key_base = '9497578f403346f031fd5ef028e2bcce2f0a9734ace7cf5d01aa83c867f101bc3de7f06bd24e41bb0d576ca7c15fcb512ec067ce8060cd1e0e615fdafe8e0cc0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FsmkCampWebsite::Application.config.session_store :cookie_store, key: '_fsmk-camp-website_session'
