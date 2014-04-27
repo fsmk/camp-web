b1189479de9d8a21c18800efc2c008c6998358de
PallaviTS <pallavi046@gmail.com>
2014-04-27 17:46:09 +0530

    Camp: Setup carousel and minor css fix

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ FsmkCampWebsite::Application.routes.draw do
 
   resources :post
   get '/about' => 'post#about'
+  get '/contact_us' => 'post#contact'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
