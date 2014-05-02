e1f2c257d788ab83d2354005f4526c52c5f7dc9e
Vishal Vijay <0vishalvijay0@gmail.com>
2014-05-02 20:54:55 +0530

    applied patch from @pallavi and showing form error from partial

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
- attr_accessible :first_name, :last_name, :gender, :college, :dept, :sem, :getting_laptop, :city, :state, :email, :phone, :previous_camp 
+ attr_accessible :first_name, :last_name, :gender, :college, :dept, :sem, :getting_laptop, :city, :state, :email, :phone, :previous_camp
  validates_presence_of :first_name, :gender, :college, :dept, :sem, :city, :state, :email, :phone
  validates :email, uniqueness: true
+ VALID_EMAIL_REGEXP = /\A^([\w\.%\+\'\-]+)@([\w\-]+\.)+([\w]{2,})$\Z/i
+ validates_format_of  :email, with: VALID_EMAIL_REGEXP, allow_blank: false
 end
