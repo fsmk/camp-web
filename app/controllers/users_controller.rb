c0331a601cbac97247fd517ff6c27d1ade5497c7
PallaviTS <pallavi046@gmail.com>
2014-05-17 15:58:50 +0530

    Minor changes in messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create
    @user = User.new(user_params)
    if @user.save
-    flash[:success] = "An email will be sent to the registered email address regarding the payment of registration fees"
+    flash[:success] = "Thank you for registrations. Your id number is #{@user.id}. A mail regarding the payment of registration fees has been sent to you. Please follow the instructions given in the same."
     redirect_to root_url
    else
     render 'new'
