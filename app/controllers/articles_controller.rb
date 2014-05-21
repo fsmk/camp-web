146f3b9d4e29e1c4398f79551a67d08035ebc0fb
Vishal Vijay <0vishalvijay0@gmail.com>
2014-05-21 23:20:32 +0530

    fix for issue #84

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,18 +4,4 @@ class ArticlesController < InheritedResources::Base
     @articles_by_date = @articles.group_by(&:event_date)
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
   end
-  def create
-    @article = Article.new article_params
-    if @article.save
-      redirect_to root_url
-    else
-      render 'new'
-    end
-  end
-
-  private
-
-  def article_params
-    params.require(:article).permit(:name, :body, :event_date, :added_by)
-  end
 end
