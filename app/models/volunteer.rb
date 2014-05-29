01214cc40a92f7ad84ca51bd2e0f2e10322d1e08
Vishal Vijay <0vishalvijay0@gmail.com>
2014-05-30 02:59:59 +0530

    volunteer registration status added

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,5 +1,5 @@
 class Volunteer < ActiveRecord::Base
-  validates_presence_of :name, :email, :gender, :sem, :college, :branch, :phone, :previous_camp, :t_shirt_size
+  validates_presence_of :name, :email, :gender, :sem, :college, :branch, :phone, :previous_camp, :t_shirt_size, :status
   validates :email, uniqueness: true
   VALID_EMAIL_REGEXP = /\A^([\w\.%\+\'\-]+)@([\w\-]+\.)+([\w]{2,})$\Z/i
   validates_format_of  :email, with: VALID_EMAIL_REGEXP, allow_blank: false
