ff013e740e3b6ef74db9a90acf3fb2204dfe7626
Vishal Vijay <0vishalvijay0@gmail.com>
2015-06-04 13:24:25 +0530

    config for static asset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ FsmkCampWebsite::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( v2014_application.css v2015_application.css  v2014_application.js  v2015_application.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
